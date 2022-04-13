5ce0f4ee88b25e4ebe7c3743052a6d5f6d7fb735
Hannah Morgan <robotooth13@gmail.com>
2022-04-13 17:27:33 +0100

    Added lots of paintings and again updated 'ruler'

--- src/main/java/net/HannahTheBeegirl/BOTWModdingMod/painting/ModPaintings.java
+++ src/main/java/net/HannahTheBeegirl/BOTWModdingMod/painting/ModPaintings.java
@@ -15,6 +15,19 @@ public class ModPaintings {
     public static final PaintingMotive FIRE = registerPainting("fire", new PaintingMotive(32, 32));
     public static final PaintingMotive WATER = registerPainting("water", new PaintingMotive(32, 32));
     public static final PaintingMotive WIND = registerPainting("wind", new PaintingMotive(32, 32));
+    public static final PaintingMotive CRAFTSMEN = registerPainting("craftsmen", new PaintingMotive(32, 16));
+    public static final PaintingMotive DESERT = registerPainting("desert", new PaintingMotive(32, 32));
+    public static final PaintingMotive ENDER = registerPainting("ender", new PaintingMotive(64, 64));
+    public static final PaintingMotive GOLEM = registerPainting("golem", new PaintingMotive(32, 32));
+    public static final PaintingMotive GOLEM2 = registerPainting("golem2", new PaintingMotive(16, 16));
+    public static final PaintingMotive MASON = registerPainting("mason", new PaintingMotive(32, 32));
+    public static final PaintingMotive MASON2 = registerPainting("mason2", new PaintingMotive(32, 32));
+    public static final PaintingMotive MASON3 = registerPainting("mason3", new PaintingMotive(16, 32));
+    public static final PaintingMotive PARENTHOOD = registerPainting("parenthood", new PaintingMotive(16, 32));
+    public static final PaintingMotive RIDERS = registerPainting("riders", new PaintingMotive(32, 32));
+    public static final PaintingMotive RUINED = registerPainting("ruined", new PaintingMotive(32, 16));
+    public static final PaintingMotive SAINTS = registerPainting("saints", new PaintingMotive(64, 64));
+    public static final PaintingMotive VILLAGERS = registerPainting("villagers", new PaintingMotive(64, 32));
 
     private static PaintingMotive registerPainting(String name, PaintingMotive paintingMotive){
         return Registry.register(Registry.PAINTING_MOTIVE, new Identifier(BOTWModdingMod.MOD_ID, name), paintingMotive);
